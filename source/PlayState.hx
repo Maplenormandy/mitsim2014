792fa3c794d7a8c0c98c3f882d6cab93d6b230eb
fox11trot <fox11trot@hotmail.com>
2014-10-06 15:36:42 -0400

    Added CampusMap file and picture asset

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,21 +11,25 @@ import flixel.util.FlxMath;
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState {
-	/**
-	 * Function that is called up when to state is created to set it up. 
+	private var _map:CampusMap;
+
+  /**
+	 * Function that is called up when to state is created to set it up.
 	 */
 	override public function create():Void {
-        Reg.studentHappiness = 0;
-        Reg.endowment = 0.0;
+    Reg.studentHappiness = 0;
+    Reg.endowment = 0.0;
 
-        var a1 = new Outcome("+1 student happiness; +$4k endowment", "");
-        a1.effect();
+    var a1 = new Outcome("+1 student happiness; +$4k endowment", "");
+    a1.effect();
 
-		super.create();
+    super.create();
+
+    _map = new CampusMap(20, 20);
 	}
-	
+
 	/**
-	 * Function that is called when this state is destroyed - you might want to 
+	 * Function that is called when this state is destroyed - you might want to
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
 	override public function destroy():Void {
@@ -37,5 +41,5 @@ class PlayState extends FlxState {
 	 */
 	override public function update():Void {
 		super.update();
-	}	
+	}
 }
