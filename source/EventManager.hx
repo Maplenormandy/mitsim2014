b25f4a8fdbc3d8ccc1c77f1f96125b3df390cc0e
Norman Cao <maplenormandy@gmail.com>
2014-10-08 14:34:45 -0400

    Added some events.

--- source/EventManager.hx
+++ source/EventManager.hx
@@ -27,7 +27,28 @@ class EventManager {
     e.addOutcome(new Outcome("+100 wealthy donors; -1mil endowment", "Yar"));
     e.addOutcome(new Outcome("-10 wealthy donors; +1mil endowment", "Nah"));
     e.addOutcome(new Outcome("+500k endowment", "Nay"));
+    this.events.add(e);
 
+
+    var e = new Event("Foot Transmitted Disease Outbreak", "All of East Campus in panic!", 1000);
+    e.addOutcome(new Outcome("-10 wealthy donors; -8 student happiness", "Make shoes mandatory"));
+    e.addOutcome(new Outcome("-120mil endowment", "Ignore it (get sued)")); 
+    this.events.add(e);
+
+    var e = new Event("Hexane Spill in 18", "Almost three whole gallons!", 500);
+    e.addOutcome(new Outcome("-5 student happiness", "Personally alert everyone on campus"));
+    e.addOutcome(new Outcome("-200k endowment", "Clean it up"));
+    e.addOutcome(new Outcome("-300mil endowment", "Ignore it (get sued)")); 
+    this.events.add(e);
+
+    var e = new Event("Opportunity", "Give $100 in BTC to every student?", 500);
+    e.addOutcome(new Outcome("+10 student happiness; +5 wealthy donors; -1mil endowment", "Yes"));
+    e.addOutcome(new Outcome("-2 student happiness", "No"));
+    this.events.add(e);
+
+    var e = new Event("Opportunity", "Hold a cupcake study break?", 800);
+    e.addOutcome(new Outcome("+4 student happiness; -1k endowment", "Yes"));
+    e.addOutcome(new Outcome("-1 student happiness", "No"));
     this.events.add(e);
 
   }
