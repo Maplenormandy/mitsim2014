2f7678115e3ca5a07fd9b66d1e550c93f027adc2
fox11trot <fox11trot@hotmail.com>
2014-10-08 16:01:38 -0400

    Added more events

--- source/EventManager.hx
+++ source/EventManager.hx
@@ -25,30 +25,41 @@ class EventManager {
 
     var e = new Event("Talk Like a Pirate", "Blub blub blub", 500);
     e.addOutcome(new Outcome("+100 wealthy donors; -1mil endowment", "Yar"));
-    e.addOutcome(new Outcome("-10 wealthy donors; +1mil endowment", "Nah"));
+    e.addOutcome(new Outcome("-20 wealthy donors; +1mil endowment", "Nah"));
     e.addOutcome(new Outcome("+500k endowment", "Nay"));
     this.events.add(e);
 
 
     var e = new Event("Foot Transmitted Disease Outbreak", "All of East Campus in panic!", 1000);
-    e.addOutcome(new Outcome("-10 wealthy donors; -8 student happiness", "Make shoes mandatory"));
-    e.addOutcome(new Outcome("-120mil endowment", "Ignore it (get sued)")); 
+    e.addOutcome(new Outcome("-20 wealthy donors; -8 student happiness", "Make shoes mandatory"));
+    e.addOutcome(new Outcome("-120mil endowment", "Ignore it (get sued)"));
     this.events.add(e);
 
     var e = new Event("Hexane Spill in 18", "Almost three whole gallons!", 500);
     e.addOutcome(new Outcome("-5 student happiness", "Personally alert everyone on campus"));
     e.addOutcome(new Outcome("-200k endowment", "Clean it up"));
-    e.addOutcome(new Outcome("-300mil endowment", "Ignore it (get sued)")); 
+    e.addOutcome(new Outcome("-300mil endowment", "Ignore it (get sued)"));
     this.events.add(e);
 
     var e = new Event("Opportunity", "Give $100 in BTC to every student?", 500);
-    e.addOutcome(new Outcome("+10 student happiness; +5 wealthy donors; -1mil endowment", "Yes"));
+    e.addOutcome(new Outcome("+10 student happiness; +15 wealthy donors; -1mil endowment", "Yes"));
     e.addOutcome(new Outcome("-2 student happiness", "No"));
     this.events.add(e);
 
     var e = new Event("Opportunity", "Hold a cupcake study break?", 800);
     e.addOutcome(new Outcome("+4 student happiness; -1k endowment", "Yes"));
     e.addOutcome(new Outcome("-1 student happiness", "No"));
+    this.events.add(e);
+
+    var e = new Event("Student falls through fraternity window", 700);
+    e.addOutcome(new Outcome("-700k endowment", "Let it slide (get sued)"));
+    e.addOutcome(new Outcome("-10 student happiness; +50k endowment", "Ban all frat parties"));
+    e.addOutcome(new Outcome("-2 student happiness; -15 wealthy donors", "Revoke charter"));
+    this.events.add(e);
+
+    var e = new Event("Parents actually see Bexley", 700);
+    e.addOutcome(new Outcome("-7 student happiness; -30 wealthy donors", "Close it down"));
+    e.addOutcome(new Outcome("-12 student happiness; -200k endowment", "Paint the walls white"));
     this.events.add(e);
 
   }
